e8e98a679730438b6f269ba06340319201ee28ff
Dickson PUN <PTS423@corp.ha.org.hk>
2021-11-25 15:50:08 +0800

    config(mongoose): set db name

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), UsersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017', {
+      dbName: 'task-manager-nestjs-api',
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
