207d73c2eb3514bfb6f926eac108abf154c54b3e
Dickson PUN <PTS423@corp.ha.org.hk>
2021-11-26 11:43:39 +0800

    chore: add mongoose scheme type options for type, required, unique, trim...

---
+++ src/common/constants/errorMessages.ts
@@ -0,0 +1,1 @@
+export const PASSWORD_MIN_LENGTH_ERR_MSG = 'Password min. length is 7 digits.';
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,20 +1,37 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { PASSWORD_MIN_LENGTH_ERR_MSG } from 'src/common/constants/errorMessages';
+import { PASSWORD_LENGTH } from '../constants';
 
 export type UserDocument = User & Document;
 
 @Schema()
 export class User {
-  @Prop()
+  @Prop({
+    type: String,
+    required: true,
+    trim: true,
+  })
   name: string;
 
-  @Prop()
+  @Prop({ type: Number, default: 0 })
   age: number;
 
-  @Prop()
+  @Prop({
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+  })
   email: string;
 
-  @Prop()
+  @Prop({
+    type: String,
+    required: true,
+    trim: true,
+    minlength: [PASSWORD_LENGTH, PASSWORD_MIN_LENGTH_ERR_MSG],
+  })
   password: string;
 }
 
