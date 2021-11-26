ea5f944551e23d1c85f3606e7dad71e7722a7400
Dickson PUN <PTS423@corp.ha.org.hk>
2021-11-26 10:20:40 +0800

    feat(users api): add update user api

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
@@ -21,5 +22,9 @@ export class UsersService {
 
   async findOne(id: string): Promise<User> {
     return await this.userModel.findOne({ _id: id });
+  }
+
+  async update(id: string, payload: UpdateUserDto): Promise<User> {
+    return await this.userModel.findOneAndUpdate({ _id: id }, payload);
   }
 }
