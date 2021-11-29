974980951ee6ae2440ab7e73df399b4dff1fcbbc
Dickson PUN <PTS423@corp.ha.org.hk>
2021-11-29 10:20:28 +0800

    chore(user validation): handle regex matches validation in CreateUserDto using class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,25 @@
 import { Transform, TransformFnParams } from 'class-transformer';
-import { IsEmail, IsNotEmpty, IsPositive, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsPositive,
+  MinLength,
+  Matches,
+  IsOptional,
+} from 'class-validator';
 import { PASSWORD_MIN_LENGTH_ERR_MSG } from 'src/common/constants/errorMessages';
 import { PASSWORD_LENGTH } from '../constants';
+
+// regex match all excludes specific word
+// https://regexland.com/regex-match-all-except/
+const excludePasswordRegex = new RegExp('^(?!.*password).*', 'i');
+
 export class CreateUserDto {
   @IsNotEmpty()
   readonly name: string;
 
   @IsPositive()
+  @IsOptional()
   readonly age: number;
 
   @IsEmail()
@@ -18,5 +31,6 @@ export class CreateUserDto {
   @MinLength(PASSWORD_LENGTH, {
     message: PASSWORD_MIN_LENGTH_ERR_MSG,
   })
+  @Matches(excludePasswordRegex)
   readonly password: string;
 }
