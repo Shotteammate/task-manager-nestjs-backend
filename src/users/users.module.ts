7efe8868b362cab481e0109fc780d075a61233cb
Dickson PUN <PTS423@corp.ha.org.hk>
2021-11-29 15:58:58 +0800

    feat(password hashing): handle password hashing by bcryptjs if password modified

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,20 +3,26 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User, UserSchema } from './schemas/user.schema';
+import * as bcrypt from 'bcryptjs';
 
 @Module({
   imports: [
     // forFeatureAsync pre-save hook must implement before MongooseModule.forFeature()
     // need to create the schema pre save hook before compiling the model
     // https://stackoverflow.com/questions/49302226/mongoose-presave-not-triggering-bcrypt
+
+    // bcrypt.hash(this['password'], 8); 8: Auto-gen a salt and hash
+    // https://www.npmjs.com/package/bcryptjs
     MongooseModule.forFeatureAsync([
       {
         name: User.name,
         useFactory: () => {
           const schema = UserSchema;
 
-          schema.pre('save', function () {
-            console.log('Hello from pre save');
+          schema.pre('save', async function () {
+            if (this.isModified('password')) {
+              this['password'] = await bcrypt.hash(this['password'], 8);
+            }
           });
           return schema;
         },
