c0a28cbc4416ce06a59fff6f6a9f97b233685fa8
Dickson PUN <PTS423@corp.ha.org.hk>
2021-11-29 15:25:53 +0800

    fix(password hashing): fix mongoose pre save hook not firing with MongooseModule.forFeatureAsync
    forFeatureAsync pre-save hook must implement before MongooseModule.forFeature()
    need to create the schema pre save hook before compiling the model
    https://stackoverflow.com/questions/49302226/mongoose-presave-not-triggering-bcrypt

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,6 +6,22 @@ import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
   imports: [
+    // forFeatureAsync pre-save hook must implement before MongooseModule.forFeature()
+    // need to create the schema pre save hook before compiling the model
+    // https://stackoverflow.com/questions/49302226/mongoose-presave-not-triggering-bcrypt
+    MongooseModule.forFeatureAsync([
+      {
+        name: User.name,
+        useFactory: () => {
+          const schema = UserSchema;
+
+          schema.pre('save', function () {
+            console.log('Hello from pre save');
+          });
+          return schema;
+        },
+      },
+    ]),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UsersController],
