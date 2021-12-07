e061d017ed349618661ecdade659d7dcead85466
Dickson PUN <PTS423@corp.ha.org.hk>
2021-12-07 11:45:04 +0800

    feat(auth refresh token): be able to implement multi-jwtStrategies with multi-secretKeys

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,25 +5,19 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import { JwtRefreshTokenStrategy } from './strategies/jwt-refresh-token.strategy';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
     ConfigModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secretOrPrivateKey: configService.get<string>('JWT_ACCESS_TOKEN_SECRET_KEY'),
-        signOptions: {
-          expiresIn: '900s', // 15 mins
-        },
-      }),
-      inject: [ConfigService],
-    }),
+    // multi-strategy with multi-secrets
+    // will always get 401 unauthorized error for refresh token if secret key initial here
+    JwtModule.register({}),
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy, JwtRefreshTokenStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
